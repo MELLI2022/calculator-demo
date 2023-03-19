c9364a61bdd09f916546746a2c96f05675cb3244
MELLI2022 <denisenkoswetlana109@gmail.com>
2023-03-19 17:24:45 +0300

    Памагите:)

---
+++ src/main/java/calculator/calculatordemo/CalculatorService.java
@@ -0,0 +1,19 @@
+package calculator.calculatordemo;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorService {
+    public int plus(int num1, int num2){
+        return num1 + num2;
+    }
+    public int minus(int num1, int num2){
+        return num1 - num2;
+    }
+    public int multiply(int num1, int num2){
+        return num1 * num2;
+    }
+    public double division(int num1, int num2){
+        return num1 /(double) num2;
+    }
+}
